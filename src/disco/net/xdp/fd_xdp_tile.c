93ac3cf9c8526597881df1afb002fcb10efdff56
Anoushk Kharangate <anoushk@tinydancer.io>
2025-07-18 19:05:05 +0530

    fix quic tile context overwrite
    Signed-off-by: Anoushk Kharangate <anoushk@tinydancer.io>

--- src/disco/net/xdp/fd_xdp_tile.c
+++ src/disco/net/xdp/fd_xdp_tile.c
@@ -207,6 +207,7 @@ typedef struct {
   fd_net_in_ctx_t in[ MAX_NET_INS ];
 
   fd_net_out_ctx_t quic_out[1];
+  fd_net_out_ctx_t pquic_out[1];
   fd_net_out_ctx_t shred_out[1];
   fd_net_out_ctx_t gossip_out[1];
   fd_net_out_ctx_t repair_out[1];
@@ -739,7 +740,7 @@ net_rx_packet( fd_net_ctx_t *      ctx,
     out = ctx->shred_out;
   } else if( FD_UNLIKELY( udp_dstport==ctx->p3_quic_transaction_listen_port ) ) {
     proto = DST_PROTO_TPU_P3_QUIC;
-    out = ctx->quic_out;
+    out = ctx->pquic_out;
   } else if( FD_UNLIKELY( udp_dstport==ctx->quic_transaction_listen_port ) ) {
     proto = DST_PROTO_TPU_QUIC;
     out = ctx->quic_out;
@@ -1171,13 +1172,18 @@ unprivileged_init( fd_topo_t *      topo,
 
   for( ulong i = 0; i < tile->out_cnt; i++ ) {
     fd_topo_link_t * out_link = &topo->links[ tile->out_link_id[ i  ] ];
-    if( strcmp( out_link->name, "net_quic" )   == 0 || 
-        strcmp( out_link->name, "net_p3quic" ) == 0 ) {
+    if( strcmp( out_link->name, "net_quic" ) == 0 ) {
       fd_topo_link_t * quic_out = out_link;
       ctx->quic_out->mcache = quic_out->mcache;
       ctx->quic_out->sync   = fd_mcache_seq_laddr( ctx->quic_out->mcache );
       ctx->quic_out->depth  = fd_mcache_depth( ctx->quic_out->mcache );
       ctx->quic_out->seq    = fd_mcache_seq_query( ctx->quic_out->sync );
+    } else if ( strcmp( out_link->name, "net_pquic" ) == 0 ) {
+      fd_topo_link_t * pquic_out = out_link;
+      ctx->pquic_out->mcache = pquic_out->mcache;
+      ctx->pquic_out->sync   = fd_mcache_seq_laddr( ctx->pquic_out->mcache );
+      ctx->pquic_out->depth  = fd_mcache_depth( ctx->pquic_out->mcache );
+      ctx->pquic_out->seq    = fd_mcache_seq_query( ctx->pquic_out->sync );
     } else if( strcmp( out_link->name, "net_shred" ) == 0 ) {
       fd_topo_link_t * shred_out = out_link;
       ctx->shred_out->mcache = shred_out->mcache;
@@ -1211,7 +1217,7 @@ unprivileged_init( fd_topo_t *      topo,
     FD_LOG_ERR(( "shred listen port set but no out link was found" ));
   } else if( FD_UNLIKELY( ctx->quic_transaction_listen_port!=0 && ctx->quic_out->mcache==NULL ) ) {
     FD_LOG_ERR(( "quic transaction listen port set but no out link was found" ));
-  } else if( FD_UNLIKELY( ctx->p3_quic_transaction_listen_port!=0 && ctx->quic_out->mcache==NULL ) ) {
+  } else if( FD_UNLIKELY( ctx->p3_quic_transaction_listen_port!=0 && ctx->pquic_out->mcache==NULL ) ) {
     FD_LOG_ERR(( "p3 quic transaction listen port set but no out link was found" ));
   } else if( FD_UNLIKELY( ctx->legacy_transaction_listen_port!=0 && ctx->quic_out->mcache==NULL ) ) {
     FD_LOG_ERR(( "legacy transaction listen port set but no out link was found" ));
